e11df72b12bd86fcb904babe13ce7d1eb54cff40
Philip Chimento <philip.chimento@gmail.com>
2015-06-06 17:10:52 -0700

    docwriter: Option to select output format
    For generating other output formats such as DevDocs-ready HTML, we add an
    output format option (-f). The default output format is "mallard" which
    leaves the existing behaviour unchanged.
    
    We can fold the existing --write-sections-file option into the new output
    format option, as a new "sections" format.

--- giscanner/docmain.py
+++ giscanner/docmain.py
@@ -25,6 +25,8 @@ from .docwriter import DocWriter
 from .sectionparser import generate_sections_file, write_sections_file
 from .transformer import Transformer
 
+FORMATS = ['mallard', 'sections']
+
 
 def doc_main(args):
     parser = optparse.OptionParser('%prog [options] GIR-file')
@@ -36,12 +38,16 @@ def doc_main(args):
                       action="store", dest="language",
                       default="c",
                       help="Output language")
+    parser.add_option("-f", "--format",
+                      action="store", dest="format",
+                      default="mallard",
+                      help="Output format [%s]" % ", ".join(FORMATS))
     parser.add_option("", "--add-include-path",
                       action="append", dest="include_paths", default=[],
                       help="include paths for other GIR files")
     parser.add_option("", "--write-sections-file",
-                      action="store_true", dest="write_sections",
-                      help="Generate and write out a sections file")
+                      action="store_const", dest="format", const="sections",
+                      help="Backwards-compatible equivalent to '-f sections'")
 
     options, args = parser.parse_args(args)
     if not options.output:
@@ -49,6 +55,10 @@ def doc_main(args):
 
     if len(args) < 2:
         raise SystemExit("Need an input GIR filename")
+
+    if options.format not in FORMATS:
+        raise SystemExit("Unknown output format %s (supported: %s)" %
+            (options.format, ", ".join(FORMATS)))
 
     if 'UNINSTALLED_INTROSPECTION_SRCDIR' in os.environ:
         top_srcdir = os.environ['UNINSTALLED_INTROSPECTION_SRCDIR']
@@ -59,14 +69,14 @@ def doc_main(args):
     extra_include_dirs.extend(options.include_paths)
     transformer = Transformer.parse_from_gir(args[1], extra_include_dirs)
 
-    if options.write_sections:
+    if options.format == 'sections':
         sections_file = generate_sections_file(transformer)
 
         fp = open(options.output, 'w')
         write_sections_file(fp, sections_file)
         fp.close()
     else:
-        writer = DocWriter(transformer, options.language)
+        writer = DocWriter(transformer, options.language, options.format)
         writer.write(options.output)
 
     return 0
